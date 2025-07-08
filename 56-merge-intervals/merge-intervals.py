2379dd0bd18b150510259a769cf3cac2663a0496
SatyamPal99 <Satyampal243ss@gmail.com>
2025-07-08 16:02:50 +0530

    Time: 2747 ms (5.13%) | Memory: 21.7 MB (14.84%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,10 +3,24 @@ class Solution:
         ans=[]
         arr.sort()
         for i in range(len(arr)):
+            st=arr[i][0]
+            end=arr[i][1]
+            if(len(ans)!=0 and end<=ans[len(ans)-1][1]):
+                continue
+            for j in range(i+1,len(arr)):
+                if arr[j][0] <= end:
+                    end=max(end,arr[j][1])
+            ans.append([st,end])
+        return ans
+
+
+        """ans=[]
+        arr.sort()
+        for i in range(len(arr)):
             if(len(ans)==0 or arr[i][0]>ans[len(ans)-1][1]):
                 st=arr[i][0]
                 end=arr[i][1]
                 ans.append([st,end])
             else:
                 ans[len(ans)-1][1]=max(ans[len(ans)-1][1],arr[i][1])
-        return ans
+        return ans"""
