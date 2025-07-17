b494f1bf772c9eeb586fcd1ae1d7dc9f16cb791d
Satyam Pal <Satyampal243ss@gmail.com>
2025-07-17 16:20:18 +0530

    Time: 15 ms (13.50%) | Memory: 18.6 MB (35.22%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1=s.lower()
+        ans=[]
+        for i in range(len(s1)):
+            if (s1[i]>='a' and s1[i]<='z') or (s1[i]>='A' and s1[i]<='Z') or (s1[i]>='0' and s1[i]<='9'):
+                ans.append(s1[i])
+        ans="".join(ans)
+        j=0
+        k=len(ans)-1
+        while(j<=k):
+            if ans[j]!=ans[k]:
+                return False
+            else:
+                j+=1
+                k-=1
+        return True
+        
+        
