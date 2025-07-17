30b6604254adbddeaf519ac59fe321af1a3e2d1d
Satyam Pal <Satyampal243ss@gmail.com>
2025-07-17 23:54:18 +0530

    Time: 6 ms (84.93%) | Memory: 18 MB (83.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s1=s.lower()
+        """s1=s.lower()
         ans=[]
         for i in range(len(s1)):
             if (s1[i]>='a' and s1[i]<='z') or (s1[i]>='A' and s1[i]<='Z') or (s1[i]>='0' and s1[i]<='9'):
@@ -14,6 +14,20 @@ class Solution:
             else:
                 j+=1
                 k-=1
+        return True"""
+
+        i=0
+        j=len(s)-1
+        while(i<j):
+            if not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1
+            elif s[i].lower()==s[j].lower():
+                i+=1
+                j-=1
+            else:
+                return False
         return True
         
         
