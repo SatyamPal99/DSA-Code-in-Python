9af55229b5a3381be52d11fe324220051739a02f
Satyam Pal <Satyampal243ss@gmail.com>
2025-08-05 23:24:47 +0530

    Time: 4 ms (63.28%) | Memory: 17.6 MB (98.66%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        prev=0
+        total=0
+        for i in reversed(s):
+            curr=d[i]
+            if curr<prev:
+                total-=curr
+            else:
+                total+=curr
+            prev=curr
+        return total
+
+        
