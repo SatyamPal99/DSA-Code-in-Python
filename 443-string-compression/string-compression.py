5ba78c6bebeac4aa338494e4583b4b27e8e219d8
Satyam Pal <Satyampal243ss@gmail.com>
2025-08-12 20:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (27.57%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,7 +1,7 @@
 class Solution:
     def compress(self, s: List[str]) -> int:
-        idx=0
-        i=0
+        """idx=0
+        i=0 
         while i<len(s):
             c=0
             ch=s[i]
@@ -15,6 +15,24 @@ class Solution:
                 for j in temp:
                     s[idx]=j
                     idx+=1
+        return idx"""
+
+        idx=0
+        i=0
+        n=len(s)
+        while(i<n):
+            j=i+1
+            while(j<n and s[i]==s[j]):
+                j+=1
+            s[idx]=s[i]
+            idx+=1
+            count=j-i
+            if (count>1):
+                temp=str(count)
+                for k in temp:
+                    s[idx]=k
+                    idx+=1
+            i=j
         return idx
             
         
