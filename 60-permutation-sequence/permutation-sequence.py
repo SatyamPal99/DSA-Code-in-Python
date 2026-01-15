b282c215b32c24aef3dde9a46a05a94f6ea37397
Satyam Pal <Satyampal243ss@gmail.com>
2026-01-16 02:06:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (12.47%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -1,6 +1,27 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         arr=[]
+        fact=1
+        for i in range(1,n):
+            fact=fact*i
+            arr.append(i)
+        arr.append(n)
+        ans=""
+        k=k-1
+        while(True):
+            ans=ans+str(arr[k//fact])
+            arr.pop(k//fact)
+            if(len(arr)==0):
+                break
+            k=k%fact
+            fact=fact//len(arr)
+        return ans
+
+
+        
+        
+        
+        """arr=[]
         for i in range(1,n+1):
             arr.append(i)
         ans=[]
@@ -17,6 +38,8 @@ class Solution:
         for i in range(idx,len(arr)):
             arr[i],arr[idx]=arr[idx],arr[i]
             self.fun(arr,ans,idx+1)
-            arr[i],arr[idx]=arr[idx],arr[i]
+            arr[i],arr[idx]=arr[idx],arr[i]"""
+
+        
 
         
