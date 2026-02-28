a9619a17fbd669e1af0860ef14573906816b1436
Satyam Pal <Satyampal243ss@gmail.com>
2026-02-28 20:46:28 +0530

    Time: 46 ms (60.68%) | Memory: 19.6 MB (59.28%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        curr=node
+        nxt=curr.next
+        curr.val=nxt.val
+        curr.next=nxt.next
+        
