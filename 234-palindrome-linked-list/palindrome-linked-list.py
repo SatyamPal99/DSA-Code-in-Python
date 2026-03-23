23417bf749783aaaba7f8cdb7b4c50f4b15204a9
Satyam Pal <Satyampal243ss@gmail.com>
2026-03-23 13:09:51 +0530

    Time: 59 ms (11.79%) | Memory: 42.5 MB (79.88%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        st=[]
+        """st=[]
         curr=head
         while curr:
             st.append(curr.val)
@@ -16,5 +16,32 @@ class Solution:
                 curr=curr.next
             else:
                 return False
+        return True"""
+        if head==None or head.next==None:
+            return True
+        slow=head
+        fast=head
+        while (fast.next!=None and fast.next.next!=None):
+            slow=slow.next
+            fast=fast.next.next
+        end=self.reverse(slow.next)
+        curr=head
+        while end!=None:
+            if curr.val==end.val:
+                curr=curr.next
+                end=end.next
+            else:
+                return False
         return True
+
+    def reverse(self,head):
+        pre=None
+        curr=temp=head
+        while curr:
+            temp=temp.next
+            curr.next=pre
+            pre=curr
+            curr=temp
+        return pre
+
         
