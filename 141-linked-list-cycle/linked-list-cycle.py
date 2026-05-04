87e0e40abd7243b20b28ea64178add154cb915f6
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-05 03:10:06 +0530

    Time: 61 ms (14.97%) | Memory: 22.4 MB (86.11%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,13 +6,27 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        temp=head
+        """temp=head
         d={}
         while temp!=None:
             d[temp]=d.get(temp,0)+1
             if d[temp]>1:
                 return True
             temp=temp.next
+        return False"""
+
+        #Optimized approach (Using Tortoise and Hare Algo)....
+        if head==None:
+            return False
+        up=head
+        down=head
+        while (up!=None and up.next!=None):
+            down=down.next
+            up=up.next.next
+            if up==down:
+                return True
         return False
+
+
 
         
