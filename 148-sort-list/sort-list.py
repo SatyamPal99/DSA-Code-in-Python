41f54159c7d69d3525675afc47bbf6dc8df638ce
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-13 01:57:57 +0530

    Time: 315 ms (5.04%) | Memory: 40.6 MB (60.03%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -19,7 +19,7 @@ class Solution:
             curr=curr.next
         return dummy.next"""
 
-        temp=head
+        """temp=head
         arr=[]
         while temp!=None:
             arr.append(temp.val)
@@ -31,6 +31,52 @@ class Solution:
             temp.val=arr[i]
             i+=1
             temp=temp.next
-        return head
+        return head"""
+
+        #optimized code (Using Inplace)
+    
+        if head==None or head.next==None:
+            return head
+        mid=self.findMid(head)
+        left_head=head
+        right_head=mid.next
+        mid.next=None
+        left_head=self.sortList(left_head)
+        right_head=self.sortList(right_head)
+        return self.merge(left_head,right_head)
+    
+    def findMid(self,head):
+        low=head
+        high=head
+        while high.next!=None and high.next.next!=None:
+            low=low.next
+            high=high.next.next
+        return low
+    def merge(self,head1,head2):
+        dummy=ListNode(-1)
+        temp=dummy
+        while head1!=None and head2!=None:
+            if head1.val<head2.val:
+                temp.next=head1
+                head1=head1.next
+                temp=temp.next
+            elif head1.val>head2.val:
+                temp.next=head2
+                head2=head2.next
+                temp=temp.next
+            else:
+                temp.next=head1
+                temp=temp.next
+                head1=head1.next
+                temp.next=head2
+                temp=temp.next
+                head2=head2.next
+        if head1==None:
+            temp.next=head2
+        elif head2==None:
+            temp.next=head1
+
+        return dummy.next
+
 
         
