5e2f3825ff2a005468f8673c5b2ed3b9b6667770
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-20 11:49:34 +0530

    Time: 91 ms (49.79%) | Memory: 23.7 MB (95.16%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -11,16 +11,16 @@ class Solution:
 
 
         n=len(arr)
-        left_s=[-1]*len(arr)
-        right_s=[n]*len(arr)
+        PS_left=[-1]*len(arr)
+        NS_right=[n]*len(arr)
         st1=[]
         for i in range(len(arr)):
             while st1 and arr[st1[-1]]>arr[i]:
                 st1.pop()
             if not st1:
-                left_s[i]=-1
+                PS_left[i]=-1
             else:
-                left_s[i]=st1[-1]
+                PS_left[i]=st1[-1]
             st1.append(i)
 
         st2=[]
@@ -28,14 +28,14 @@ class Solution:
             while st2 and arr[st2[-1]]>=arr[i]:
                 st2.pop()
             if not st2:
-                right_s[i]=n
+                NS_right[i]=n
             else:
-                right_s[i]=st2[-1]
+                NS_right[i]=st2[-1]
             st2.append(i)
         total=0 
         for i in range(len(arr)):
-            lmax=i-left_s[i]
-            rmax=right_s[i]-i
+            lmax=i-PS_left[i]
+            rmax=NS_right[i]-i
             total=(total+(lmax*rmax)*arr[i])%(10**9+7)
-        return total%(10**9+7)
+        return total
         
