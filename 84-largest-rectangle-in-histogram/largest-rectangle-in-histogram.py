94428875a4b6ccdbefa964c93f0d5388e3504c91
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-24 20:44:38 +0530

    Time: 125 ms (44.65%) | Memory: 31.2 MB (61.54%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n=len(heights)
+        """n=len(heights)
         LSE=[-1]*n
         RSE=[n]*n
         st1=[]
@@ -26,7 +26,24 @@ class Solution:
         maxi=-9999
         for i in range(len(heights)):
             maxi=max(maxi,heights[i]*(RSE[i]-LSE[i]-1))
-        return maxi
+        return maxi"""
 
-
-        
+        #Optimized
+        n=len(heights)
+        area=0
+        st=[]
+        for i in range(len(heights)):
+            while st and heights[st[-1]]>heights[i]:
+                ele=st[-1]
+                st.pop()
+                NSE=i
+                PSE=st[-1] if st else -1
+                area=max(area,heights[ele]*(NSE-PSE-1))
+            st.append(i)
+        while st:
+            NSE=n
+            ele=st[-1]
+            st.pop()
+            PSE=st[-1] if st else -1
+            area=max(area,heights[ele]*(NSE-PSE-1))
+        return area
