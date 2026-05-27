8c0202e6c1da03cbd6d84291a550123d50b4914f
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-27 11:00:41 +0530

    Time: 76 ms (26.16%) | Memory: 24.6 MB (6.71%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.py
@@ -0,0 +1,25 @@
+class StockSpanner:
+
+    def __init__(self): #when object created then by default constructor returns None..
+        self.prices = []
+        self.st=[]
+        self.ans=[]
+
+    def next(self, price: int) -> int:
+        """self.prices.append(price)
+        i=len(self.prices)-1
+        span=0
+        while i>=0 and self.prices[i]<=price:
+            span+=1
+            i-=1
+        return span"""
+
+
+        self.prices.append(price)
+        span=1
+        while self.st and self.st[-1][0]<=price:
+            ele=self.st.pop()
+            span=span+ele[1]
+        self.st.append([price,span])
+        return span
+        
