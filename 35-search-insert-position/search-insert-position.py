e259d5436a11ed118998cd03f2f5fd1e6fbc4a89
Satyam Pal <Satyampal243ss@gmail.com>
2026-05-31 18:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (42.97%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,11 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], tar: int) -> int:
-        """if len(nums)==1:
-            if nums[0]>=tar:
-                return 0
-            elif nums[0]<tar:
-                return 1"""
-        i=0
+        # Using Binary Search
+        """i=0
         j=len(nums)-1
         while(i<=j):
             mid=(i+j)//2
@@ -18,5 +14,18 @@ class Solution:
         if nums[mid]>tar:
             return mid
         elif nums[mid]<tar:
-            return mid+1
+            return mid+1"""
+
+        # Using Lower Bound
+        i=0
+        j=len(nums)-1
+        ans=len(nums)
+        while(i<=j):
+            mid=(i+j)//2
+            if nums[mid]>=tar:
+                ans=mid
+                j=mid-1
+            else:
+                i=mid+1
+        return ans
         
