9d0069ab3c1e996f49291d64e2ea8fb421d994ba
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-06 01:32:26 +0530

    Time: 4 ms (1.49%) | Memory: 19.2 MB (96.50%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -7,6 +7,19 @@ class Solution:
             return 0
         if nums[n-1]>nums[n-2]:
             return n-1
+        for i in range(1,n-1):
+            if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
+                return i
+
+
+
+
+        """if n==1:
+            return 0
+        if nums[0]>nums[1]:
+            return 0
+        if nums[n-1]>nums[n-2]:
+            return n-1
         low=1
         high=n-2
         while(low<=high):
@@ -17,5 +30,5 @@ class Solution:
                 low=mid+1
             elif nums[mid]<nums[mid-1]:
                 high=mid-1
-        return mid
+        return mid"""
         
