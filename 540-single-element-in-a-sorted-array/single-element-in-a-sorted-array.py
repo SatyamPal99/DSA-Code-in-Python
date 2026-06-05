7935594199fcd9585f3f127bab0f1e117f2b9593
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-05 23:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (28.60%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -2,7 +2,7 @@ class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         # Bruite force
 
-        # when only input array have 1 element...
+        """# when only input array have 1 element...
         if len(nums)==1:
             return nums[0]
         for i in range(len(nums)):
@@ -17,23 +17,25 @@ class Solution:
             # for not first and not last element...
             else:
                 if nums[i]!=nums[i-1] and nums[i]!=nums[i+1]:
-                    return nums[i]
+                    return nums[i]"""
         
         #Optimized Code....
         n=len(nums)
-        if len(nums)==1:
+        if n==1:
             return nums[0]
         if nums[0]!=nums[1]:
             return nums[0]
         if nums[n-1]!=nums[n-2]:
             return nums[n-1]
-        low=0
+        low=1
         high=n-2
         while(low<=high):
             mid=(low+high)//2
-            if mid%2!=0 and nums[mid-1]==nums[mid]:
-                low=mid+1
-            else:
+            if nums[mid-1]!=nums[mid] and nums[mid+1]!=nums[mid]:
+                return nums[mid]
+            if (mid%2!=0 and nums[mid+1]==nums[mid]) or (mid%2==0 and nums[mid]==nums[mid-1]):
                 high=mid-1
+            elif (mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2!=0 and nums[mid]==nums[mid-1]):
+                low=mid+1
         return nums[mid]
 
