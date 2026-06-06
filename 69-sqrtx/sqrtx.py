391e498d29630337d365a62039053161ee64802e
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-06 13:29:59 +0530

    Time: 1172 ms (14.82%) | Memory: 19.1 MB (87.87%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,5 +1,16 @@
 import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return math.isqrt(x)
+        #using math library(sqrt(x) give float so use isqrt(x) for int )....
+        """return math.isqrt(x)"""
+
+        ans=0
+        for i in range(1,x+1):
+            if i*i<=x:
+                ans=i
+            else:
+                break
+        return ans
+        
+
         
