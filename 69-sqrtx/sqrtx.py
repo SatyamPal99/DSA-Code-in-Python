970428665db432f2f4395db4cec7dd7935d4fbf4
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-06 13:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (58.41%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,13 +4,31 @@ class Solution:
         #using math library(sqrt(x) give float so use isqrt(x) for int )....
         """return math.isqrt(x)"""
 
-        ans=0
+
+        # Iterative soluiton.... (tc=O(n))
+        """ans=0
         for i in range(1,x+1):
             if i*i<=x:
                 ans=i
             else:
                 break
+        return ans"""
+
+
+        #using binary search....
+
+        low=1
+        high=x
+        ans=0
+        while(low<=high):
+            mid=(low+high)//2
+            if mid*mid>x:
+                high=mid-1
+            else:
+                ans=mid
+                low=mid+1
         return ans
-        
+            
+
 
         
