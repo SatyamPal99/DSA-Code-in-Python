7592a5814b389737c1248fdeecffba8951947b71
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-10 16:44:57 +0530

    Time: 6 ms (16.65%) | Memory: 19.5 MB (77.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        i=0
+        """i=0
         j=0
         new=[]
         while(i<len(nums1) and j<len(nums2)):
@@ -28,5 +28,57 @@ class Solution:
         elif len(new)%2==0:
             first=len(new)//2
             second=first-1
-            return (new[first]+new[second])/2
+            return (new[first]+new[second])/2"""
+
+        # Optimized code...
+        n=len(nums1)+len(nums2)
+        idx2=n//2
+        idx1=idx2-1
+        i=0
+        j=0
+        count=0
+        ele1=-1
+        ele2=-1
+        while(i<len(nums1) and j<len(nums2)):
+            if nums1[i]<nums2[j]:
+                if count==idx1:
+                    ele1=nums1[i]
+                if count==idx2:
+                    ele2=nums1[i]
+                count+=1
+                i+=1
+            elif nums1[i]>nums2[j]:
+                if count==idx2:
+                    ele2=nums2[j]
+                if count==idx1:
+                    ele1=nums2[j]
+                count+=1
+                j+=1
+            else:
+                if count==idx1:
+                    ele1=nums1[i]
+                if count==idx2:
+                    ele2=nums1[i]
+                count+=1
+                i+=1
+        
+        while(i<len(nums1)):
+            if count==idx1:
+                ele1=nums1[i]
+            if count==idx2:
+                ele2=nums1[i]
+            count+=1
+            i+=1
+
+        while(j<len(nums2)):
+            if count==idx1:
+                ele1=nums2[j]
+            if count==idx2:
+                ele2=nums2[j]
+            count+=1
+            j+=1
+        if n%2==0:
+            return (ele1+ele2)/2
+        else:
+            return ele2
         
