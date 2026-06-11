52894044b6b54146083b0b2527f0f1abaef53a48
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-11 15:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (15.20%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -30,8 +30,8 @@ class Solution:
             second=first-1
             return (new[first]+new[second])/2"""
 
-        # Optimized code...
-        n=len(nums1)+len(nums2)
+        # Space Optimized code ...
+        """n=len(nums1)+len(nums2)
         idx2=n//2
         idx1=idx2-1
         i=0
@@ -80,5 +80,39 @@ class Solution:
         if n%2==0:
             return (ele1+ele2)/2
         else:
-            return ele2
+            return ele2"""
+
+        # Optimized code....
+        n1=len(nums1)
+        n2=len(nums2)
+        if n1>n2:
+            return self.findMedianSortedArrays(nums2,nums1)
+        low=0
+        high=n1
+        left=(n1+n2+1)//2   
+        n=n1+n2
+        while(low<=high):
+            mid1=(low+high)//2
+            mid2=left-mid1
+            l1=l2=-(math.inf)
+            r1=r2=math.inf
+            if mid1<n1:
+                r1=nums1[mid1]
+            if mid2<n2:
+                r2=nums2[mid2]
+            if mid1-1>=0:
+                l1=nums1[mid1-1]
+            if mid2-1>=0:
+                l2=nums2[mid2-1]
+            if l1<=r2 and l2<=r1:
+                if n%2==1:
+                    return max(l1,l2)
+                return (max(l1,l2)+min(r1,r2))/2
+            elif l1>r2:
+                high=mid1-1
+            else:
+                low=mid1+1
+        return 0
+                
+
         
