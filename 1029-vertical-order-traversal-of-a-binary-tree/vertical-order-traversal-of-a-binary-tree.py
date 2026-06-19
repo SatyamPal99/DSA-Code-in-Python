3b4c257788985f9ed33a2a3c1a3e95975cac7122
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-19 13:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (85.48%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -7,7 +7,32 @@ from collections import defaultdict , deque
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        mapp = defaultdict(lambda:defaultdict(list)) # if a key not present then append emptylist        
+        mapp=defaultdict(list)
+        self.helper(0,0,root,mapp)
+        res=[]
+        for i in sorted(mapp.keys()):
+            temp=[]
+            for j in sorted(mapp[i]):
+                temp.append(j[1])
+            res.append(temp)
+        return res
+
+
+    def helper(self,verticle,level,node,mapp):
+        if node==None:
+            return
+        mapp[verticle].append((level,node.val))
+        self.helper(verticle-1,level+1,node.left,mapp)
+        self.helper(verticle+1,level+1,node.right,mapp)
+
+
+
+
+
+
+
+
+        """mapp = defaultdict(lambda:defaultdict(list)) # if a key not present then append emptylist        
         q=deque([(root,(0,0))]) #list of tuples inside queue
         while q:
             node,(x,y)=q.popleft()
@@ -22,4 +47,4 @@ class Solution:
             for y in sorted(mapp[x]):   # sorting inside x on the basis of y...
                 col.extend(sorted(mapp[x][y]))   # append value on nested key [x][y]--> value
             ans.append(col)
-        return ans
+        return ans"""
