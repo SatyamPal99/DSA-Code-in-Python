9338c1b4cedfcc4d90f93ca247ec6529c0a22649
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-21 18:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.60%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if root==None or (root.left==None and root.right==None):
+        """if root==None or (root.left==None and root.right==None):
             return True
         if root.left==None or root.right==None:
             return False
@@ -31,7 +31,24 @@ class Solution:
                 q2.append((node2.right,x2+1))
             if node2.left:
                 q2.append((node2.left,x2-1))
-        return True
+        return True"""
+        if root.left==root.right==None:
+            return True
+        if (root.left==None or root.right==None):
+            return False
+
+        if root.left.val!=root.right.val:
+            return False
+        return self.fun(root.left,root.right)
+    def fun(self,left_root,right_root):
+        if left_root==None or right_root==None:
+            return left_root == right_root
+   
+        if left_root.val!=right_root.val:
+            return False
+        t1=self.fun(left_root.left,right_root.right)
+        t2=self.fun(left_root.right,right_root.left)
+        return t1 and t2
 
 
         
