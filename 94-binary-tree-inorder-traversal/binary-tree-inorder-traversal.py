421d9d43a4285fd885fe19fcdd60a19f64ff882d
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-27 19:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (26.36%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,8 +6,37 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res=[]
-        return self.fun(root,res)
+        """res=[]
+        return self.fun(root,res)"""
+
+        # Morris Traversal
+
+        ans=[]
+        curr=root
+        while curr:
+            if curr.left==None:
+                ans.append(curr.val)
+                curr=curr.right
+            else:
+                predecessor=self.find_p(curr)
+                if predecessor.right is None:
+                    predecessor.right=curr
+                    curr=curr.left
+                else:
+                    predecessor.right=None
+                    ans.append(curr.val)
+                    curr=curr.right
+        return ans
+
+    def find_p(self,curr):
+        pred=curr.left
+        while pred.right and pred.right!=curr:
+            pred=pred.right
+        return pred
+
+
+
+
     def fun(self,node,res):
         if node==None:
             return []
