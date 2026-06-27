58bc25f6ed41a90731f7d5dc0a62885528b67235
Satyam Pal <Satyampal243ss@gmail.com>
2026-06-27 17:51:00 +0530

    Time: 81 ms (89.99%) | Memory: 22.3 MB (86.66%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        if root==None:
+            return ""
+        q=deque()
+        ans=""
+        q.append(root)
+        while q:
+            temp=q.popleft()
+            if temp==None:
+                ans=ans+('#,')
+            else:
+                ans=ans+(str(temp.val)+',')
+            if temp!=None:
+                q.append(temp.left)
+                q.append(temp.right)
+        print(ans)
+        return ans         
+
+    def deserialize(self, data):
+        if not data:
+            return None
+        
+        nodes=data.split(',')[:-1]  # str to comma seperated list of char...
+        root=TreeNode(int(nodes[0]))
+        q=deque([root])
+        i=1
+        while q and i<len(nodes):
+            curr=q.popleft()
+            if nodes[i]!='#':
+                curr.left=TreeNode(int(nodes[i]))
+                q.append(curr.left)
+            i+=1
+            if i<len(nodes) and nodes[i]!='#':
+                curr.right=TreeNode(int(nodes[i]))
+                q.append(curr.right)
+            i+=1
+        return root
+        
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
