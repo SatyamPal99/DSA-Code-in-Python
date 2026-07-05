b3460594686b7df5537717a484319bb2450489e1
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-05 18:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (93.32%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/recover-binary-search-tree.py
@@ -6,7 +6,8 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        ans=[]
+        # Bruteforce Approach
+        """ans=[]
         q=deque([root])
         while q:
             temp=q.popleft()
@@ -17,7 +18,37 @@ class Solution:
                 q.append(temp.right)
         ans.sort()
         print(ans)
-        return self.helper(root,ans)
+        return self.helper(root,ans)"""
+
+        # Optimized Approach....
+
+        self.first=None
+        self.middle=None
+        self.last=None
+        self.prev=None
+        self.fun(root)
+        if self.first and self.last:
+            self.first.val,self.last.val=self.last.val,self.first.val
+        else:
+            self.first.val,self.middle.val=self.middle.val,self.first.val
+        
+
+
+    def fun(self,root):
+        if root==None:
+            return
+        self.fun(root.left)
+        if self.prev!=None and self.prev.val>root.val:
+            if self.first==None:
+                self.first=self.prev
+                self.middle=root
+            else:
+                self.last=root
+        self.prev=root
+
+        self.fun(root.right)
+        
+
 
     def helper(self,root,ans):
         curr=root
