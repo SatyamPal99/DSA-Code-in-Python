d22712765c9a8676c79679769ee38a020a566d84
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-09 22:05:32 +0530

    Time: 209 ms (17.96%) | Memory: 26 MB (13.13%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        """maxlen=0
+        for i in range(len(fruits)):
+            mapp=set()
+            for j in range(i,len(fruits)):
+                mapp.add(fruits[j])
+                if len(mapp)<=2:
+                    maxlen=max(maxlen,j-i+1)
+                else:
+                    break
+        return maxlen"""
+
+        mapp={}
+        l=0
+        r=0
+        maxlen=0
+        while(r<len(fruits)):
+            mapp[fruits[r]]=mapp.get(fruits[r],0)+1
+            while len(mapp)>2:
+                mapp[fruits[l]]-=1
+                if mapp[fruits[l]]==0:
+                    del mapp[fruits[l]]
+                l+=1
+            maxlen=max(maxlen,r-l+1)
+            r+=1
+        return maxlen
+
