7824bf8fdc12f0440319026e626c4534dc381bae
Satyam Pal <Satyampal243ss@gmail.com>
2026-07-17 01:38:39 +0530

    Time: 1 ms (84.44%) | Memory: 19.2 MB (78.53%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -4,11 +4,14 @@ class Solution:
         m=len(grid[0])
         vis=[[0]*m for _ in range(n)]
         q=deque()
+        count=0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]==2:
                     q.append([i,j,0])
                     vis[i][j]=1
+                if grid[i][j]==1:
+                    count+=1
         time=0
         while q:
             temp=q.popleft()
@@ -24,10 +27,10 @@ class Solution:
                 if row<n and col<m and row>=0 and col>=0 and vis[row][col]==0 and grid[row][col]==1:
                     q.append([row,col,t+1])
                     vis[row][col]=1
-        for i in range(n):
-            for j in range(m):
-                if vis[i][j]==0 and grid[i][j]==1:
-                    return -1
+                    count-=1
+        if count!=0:
+            return -1
+        
         return time
 
 
