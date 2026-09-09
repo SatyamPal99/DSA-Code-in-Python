0591d309fb05869e6224f893074770389716d0b0
Satyam Pal <Satyampal243ss@gmail.com>
2026-09-09 05:48:53 +0530

    Time: 15 ms (10.97%) | Memory: 20.5 MB (56.49%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,6 +1,44 @@
+class Disjoint_set:
+    def __init__(self,n):
+        self.par=list(range(n+1))
+        self.rank=[0]*(n+1)
+        self.size=[1]*(n+1)
+    def find_set(self,node):
+        if node==self.par[node]:
+            return node
+        self.par[node]=self.find_set(self.par[node])
+        return self.par[node]
+
+    def union_by_size(self,u,v):
+        root_u=self.find_set(u)
+        root_v=self.find_set(v)
+        if root_u==root_v:
+            return False
+        if self.size[root_v]<self.size[root_u]:
+            self.par[root_v]=root_u
+            self.size[root_u]=self.size[root_u]+self.size[root_v]
+        else:
+            self.par[root_u]=root_v
+            self.size[root_v]=self.size[root_v]+self.size[root_u]
+        return True
+
+    def union_by_rank(self,u,v):
+        root_u=self.find_set(u)
+        root_v=self.find_set(v)
+        if root_u==root_v:
+            return
+        if self.rank[root_u]<self.rank[root_v]:
+            self.par[u]=self.par[v]
+        elif self.rank[root_u]>self.rank[root_v]:
+            self.par[v]=self.par[u]
+        else:
+            self.par[v]=self.par[u]
+            self.rank[root_u]+=1
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        v=len(isConnected)
+        """v=len(isConnected)
         adj=[[] for _ in range(v)]
         for l in range(v):
             for m in range(v):
@@ -25,12 +63,24 @@ class Solution:
             for k in adj[temp]:
                 if vis[k]==0:
                     q.append(k)
-                    vis[k]=1
+                    vis[k]=1"""
+
+        n=len(isConnected)
+        ds=Disjoint_set(n)
+
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    ds.union_by_size(i,j)
                     
-                
+        components=0
+        for node in range(n):
+            if ds.find_set(node)==node:
+                components+=1
+        return components
 
 
 
 
 
-        
+   
