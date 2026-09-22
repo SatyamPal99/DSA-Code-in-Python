c0fe43499abe4363572beee63fc642efefd31196
Satyam Pal <Satyampal243ss@gmail.com>
2026-09-22 23:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (25.33%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -6,7 +6,7 @@ class Solution:
 
         #Tabular DP
     
-        dp=[-1]*(n+1)
+        """dp=[-1]*(n+1)
 
         if n==1:
             return nums[0]
@@ -20,7 +20,30 @@ class Solution:
         for i in range(3,n):
             dp[i]=max(dp[i-2]+nums[i],dp[i-3]+nums[i])
             ans=max(ans,dp[i])
+        return ans"""
+
+        #Space Optimization...
+        n=len(nums)
+
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
+        if n==3:
+            return max(nums[1],nums[0]+nums[2])
+
+        prev3=nums[0]
+        prev2=nums[1]
+        prev1=max(nums[1],nums[0]+nums[2])
+        ans=max(prev1,prev2,prev3)
+        for i in range(3,n):
+            curr=max(prev2+nums[i],prev3+nums[i])
+            prev3=prev2
+            prev2=prev1
+            prev1=curr
+            ans=max(ans,curr)
         return ans
+
 
     def fun(self,n,nums,dp):
         if n==0:
