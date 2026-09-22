4124eaf8b7ab3d660b3b14a8e3da542c69e3cfc2
Satyam Pal <Satyampal243ss@gmail.com>
2026-09-22 23:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (90.76%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,26 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
         n=len(nums)
+        """dp=[-1]*(n+1)
+        return self.fun(n-1,nums,dp)"""
+
+        #Tabular DP
+    
         dp=[-1]*(n+1)
-        return self.fun(n-1,nums,dp)
+
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
+
+        dp[0]=nums[0]
+        dp[1]=nums[1]
+        dp[2]=dp[0]+nums[2]
+        ans=max(dp[2],dp[1])
+        for i in range(3,n):
+            dp[i]=max(dp[i-2]+nums[i],dp[i-3]+nums[i])
+            ans=max(ans,dp[i])
+        return ans
 
     def fun(self,n,nums,dp):
         if n==0:
